4dd288132e1335f40eeec79dfc97add4d3003318
Aaron Bodell <aaron.bodell@gmail.com>
2025-07-23 20:05:50 -0600

    Initial Commit

---
+++ FootballStatsV3Tests/FootballStatsV3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  FootballStatsV3Tests.swift
+//  FootballStatsV3Tests
+//
+//  Created by Aaron Bodell on 7/23/25.
+//
+
+import Testing
+@testable import FootballStatsV3
+
+struct FootballStatsV3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
